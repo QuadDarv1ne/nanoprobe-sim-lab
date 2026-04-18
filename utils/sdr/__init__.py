d62f8884d06f4a9fe8aca781c402327c94f927e8
QuadDarv1ne <maksimqwe42@mail.ru>
2026-04-18 17:18:28 +0300

    refactor: добавить экспорты новых модулей в utils/sdr/__init__.py

--- utils/sdr/__init__.py
+++ utils/sdr/__init__.py
@@ -9,6 +9,9 @@ Ring buffer and trigger recording.
 from .meteor_capture import MeteorCaptureManager
 from .noaa_capture import NOAACaptureManager
 from .ring_buffer import SharedRingBuffer
+from .rtl_sdr_auto_calibration import RTLSDRAutoCalibration
+from .rtl_sdr_calibration import RTLSDRCalibrator
+from .satellite_auto_capture import SatelliteAutoCapture
 from .sdr_resource_manager import (
     PRIORITY_ISS,
     PRIORITY_SCAN,
@@ -22,6 +25,7 @@ from .sdr_resource_manager import (
 from .trigger_recorder import TriggerRecorder
 
 __all__ = [
+    # Resource management
     "SDRResourceManager",
     "get_sdr_resource_manager",
     "SDRTask",
@@ -30,8 +34,15 @@ __all__ = [
     "PRIORITY_WEATHER",
     "PRIORITY_SSTV",
     "PRIORITY_SCAN",
+    # Capture managers
     "NOAACaptureManager",
     "MeteorCaptureManager",
+    "SatelliteAutoCapture",
+    # Ring buffer
     "SharedRingBuffer",
+    # Trigger recorder
     "TriggerRecorder",
+    # Calibration
+    "RTLSDRCalibrator",
+    "RTLSDRAutoCalibration",
 ]
